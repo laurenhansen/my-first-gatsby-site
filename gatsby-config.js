22dada9bb875141183b1ac5771dd783e9629b590
Lauren Hansen <lauren.hansen@gmail.com>
2022-08-11 12:32:50 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `My First Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-BH6CKPZYPS"
+    }
+  }]
+};
